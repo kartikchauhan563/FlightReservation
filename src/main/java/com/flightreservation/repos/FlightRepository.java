4224167e260932a6eb4e392279f6a64b20d71fb9
Kartik <kartik_chauhan@persistent.com>
2022-04-05 22:33:27 +0530

    Flight reservation is going on

--- src/main/java/com/flightreservation/repos/FlightRepository.java
+++ src/main/java/com/flightreservation/repos/FlightRepository.java
@@ -1,9 +1,16 @@
 package com.flightreservation.repos;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.flightreservation.entities.Flight;
 
-public interface FlightRepository extends JpaRepository<Flight, Long>{
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+	@Query("from Flight where departureCity=:departureCity and arrivalCity=:arrivalCity and dateOfDeparture=:dateOfDeparture")
+	List<Flight> findFlights(String from, String to, Date departureDate);
 
 }
